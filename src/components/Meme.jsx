655b2ee861559cc105160b2dc56915d10b014e56
Yuriy-Bolotnyy <yuriy.bolotnyy.cohort225@gmail.com>
2022-10-15 15:34:55 -0700

    Refactor input form

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -4,9 +4,11 @@ import "./Meme.css"
 const Meme = () => {
     return (
         <div className="inputForm">
-            <input className="header" type="text" placeholder="Shut Up"/>
-            <input className="footer" type="text" placeholder="and take"/>
-            <button>Get a new meme image</button>
+            <div className="textInputContainer">
+                <input className="header" type="text" placeholder="Shut Up"/>
+                <input className="footer" type="text" placeholder="and take"/>
+            </div>
+                <button>Get a new meme image</button>
         </div>
     )
 }
