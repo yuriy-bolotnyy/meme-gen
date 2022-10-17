47d883528e44375cab8c7b6c534e67d0c9675b82
Yuriy-Bolotnyy <yuriy.bolotnyy.cohort225@gmail.com>
2022-10-16 17:45:40 -0700

    Add state

--- src/components/MemeGrid.jsx
+++ src/components/MemeGrid.jsx
@@ -4,7 +4,12 @@ import "./MemeGrid.css"
 import memesData from "../memesData";
 
 const MemeGrid = () => {
-    const [topText, bottomText, randomImage] = React.useState(["", "", "https://i.imgflip.com/tau4.jpg"])
+    const [meme, setMeme] = React.useState({
+        topText: "Top text", 
+        bottomText: "Bottom text", 
+        imgUrl: "https://i.imgflip.com/tau4.jpg"})
+
+    console.log(`Top text: ${meme.topText} | Bottom text: ${meme.bottomText} | img:${meme.imgUrl}`)
 
     let randomIndex = Math.floor(Math.random()*memesData.data.memes.length)
     console.dir(memesData.data.memes[randomIndex].url)
@@ -16,21 +21,26 @@ const MemeGrid = () => {
 
     const cnahgeImg = () => {
         // document.querySelector(".imgContainer>img").src = randomImg()
-        setImgUrl(randomImg())
+        // setImgUrl(randomImg())
+        setMeme(prevMeme => ({...prevMeme, imgUrl:randomImg()}))
+
+        console.log(`updated to ---> Top text: ${meme.topText} | Bottom text: ${meme.bottomText} | img:${meme.imgUrl}`)
+
     }
 
-    const [imgUrl, setImgUrl] = React.useState('')
+    // const [imgUrl, setImgUrl] = React.useState('')
 
 
     return (
         <div className="inputGrid">
-            <input className="top" type="text" placeholder="Top text"/>
-            <input className="bottom" type="text" placeholder="Bottom text"/>
+            <input className="top" type="text" placeholder={meme.topText}/>
+            <input className="bottom" type="text" placeholder={meme.bottomText}/>
             <button onClick={cnahgeImg}>Get a new meme image</button>
 
             <div className="imgContainer">
                 {/* <img src="https://i.imgflip.com/emccr.jpg" alt="image" /> */}
-                <img src={randomImg()} alt="image" />
+                {/* <img src={randomImg()} alt="image" /> */}
+                <img src={meme.imgUrl} alt="image" />
                 {/* <img src={imgUrl} alt="imageUrl" /> */}
             </div>
             
